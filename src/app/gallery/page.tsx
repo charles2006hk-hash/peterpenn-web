dad3c41ccaa7d04eb6b28888af1c7e6f4ce29df9
charles2006hk-hash <charles2006hk@gmail.com>
2026-04-17 18:09:16 +0800

    Update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -7,7 +7,7 @@ import { db } from "@/lib/firebase";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
-import { ArrowLeft, Loader2, X } from "lucide-react";
+import { ArrowLeft, ArrowRight, Loader2, X } from "lucide-react";
 
 interface Work {
   id: string;
